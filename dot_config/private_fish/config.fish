766c815d5eb19b59fce0a6e80311231c484c2a4c
Graeme Read <graeme@sigma957.net>
2024-02-05 07:51:03 +0000

    Add colour to man pages 🚀

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -21,6 +21,16 @@ set __fish_git_prompt_showstashstate ''
 set __fish_git_prompt_showupstream 'none'
 set -g fish_prompt_pwd_dir_length 3
 
+# Coloured man output
+# From http://linuxtidbits.wordpress.com/2009/03/23/less-colors-for-man-pages/
+setenv LESS_TERMCAP_mb \e'[01;31m'       # begin blinking
+setenv LESS_TERMCAP_md \e'[01;38;5;74m'  # begin bold
+setenv LESS_TERMCAP_me \e'[0m'           # end mode
+setenv LESS_TERMCAP_se \e'[0m'           # end standout-mode
+setenv LESS_TERMCAP_so \e'[38;5;246m'    # begin standout-mode - info box
+setenv LESS_TERMCAP_ue \e'[0m'           # end underline
+setenv LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
+
 function fish_prompt
 	set_color brblack
 	echo -n "["(date "+%H:%M")"] "
