66c2e8b38a1eed3b14e3a3d718cc6f665c5910fd
Graeme Read <graeme@sigma957.net>
2024-02-13 13:59:27 +0000

    Make use of lazy.nvim for managing nvim plugins

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,5 +1,15 @@
 -- Seeded from https://github.com/jonhoo/configs/blob/master/editor/.config/nvim/init.lua
 
+-- always set leader first!
+-- vim.keymap.set("n", "<Space>", "<Nop>", { silent = true })
+vim.g.mapleader = " "
+
+-------------------------------------------------------------------------------
+--
+-- preferences
+--
+-------------------------------------------------------------------------------
+
 -- sweet sweet relative line numbers
 vim.opt.relativenumber = true
 
@@ -9,3 +19,23 @@ vim.opt.number = true
 -- infinite undo!
 -- NOTE: ends up in ~/.local/state/nvim/undo/
 vim.opt.undofile = true
+
+-------------------------------------------------------------------------------
+--
+-- plugin configuration
+--
+-------------------------------------------------------------------------------
+-- first, grab the manager
+-- https://github.com/folke/lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
