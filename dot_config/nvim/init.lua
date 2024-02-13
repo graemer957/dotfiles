48ccf4bea1637055a1a59e96a8e1303002ec0f8f
Graeme Read <graeme@sigma957.net>
2024-02-13 16:54:11 +0000

    Add language support for rust, terraform, toml, yaml and markdown

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -97,7 +97,53 @@ require("lazy").setup({
 			)
 		end
 	},
+	-- language support
+	-- terraform
+	{
+		'hashivim/vim-terraform',
+		ft = { "terraform" },
+	},
+	-- toml
+	'cespare/vim-toml',
+	-- yaml
+	{
+		"cuducos/yaml.nvim",
+		ft = { "yaml" },
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+		},
+	},
+	-- rust
+	{
+		'rust-lang/rust.vim',
+		ft = { "rust" },
+		config = function()
+			vim.g.rustfmt_autosave = 1
+			vim.g.rustfmt_emit_files = 1
+			vim.g.rustfmt_fail_silently = 0
+			vim.g.rust_clip_command = 'pbcopy'
+		end
+	},
 	-- fish
 	'khaveesh/vim-fish-syntax',
+	-- markdown
+	{
+		'plasticboy/vim-markdown',
+		ft = { "markdown" },
+		dependencies = {
+			'godlygeek/tabular',
+		},
+		config = function()
+			-- never ever fold!
+			vim.g.vim_markdown_folding_disabled = 1
+			-- support front-matter in .md files
+			vim.g.vim_markdown_frontmatter = 1
+			-- 'o' on a list item should insert at same level
+			vim.g.vim_markdown_new_list_item_indent = 0
+			-- don't add bullets when wrapping:
+			-- https://github.com/preservim/vim-markdown/issues/232
+			vim.g.vim_markdown_auto_insert_bullets = 0
+		end
+	},
 })
 
