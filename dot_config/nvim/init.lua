6ba91db97d8ede6ae209d410b4dd0b0e2332204f
Graeme Read <graeme@sigma957.net>
2024-02-24 16:33:20 +0000

    Set up Ctrl-P for searching for files in neovim
    Makes use of fzf (https://github.com/junegunn/fzf) and proximity-sort (https://github.com/jonhoo/proximity-sort)

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -92,6 +92,8 @@ vim.opt.termguicolors = true
 -- hotkeys
 --
 -------------------------------------------------------------------------------
+-- quick-open
+vim.keymap.set('', '<C-p>', '<cmd>Files<cr>')
 -- quick-save
 vim.keymap.set('n', '<leader>w', '<cmd>w<cr>')
 
@@ -180,6 +182,35 @@ require("lazy").setup({
 				]],
 				true
 			)
+		end
+	},
+	-- fzf support for ^p
+	{
+		'junegunn/fzf.vim',
+		dependencies = {
+			{ 'junegunn/fzf', dir = '~/.fzf', build = './install --all' },
+		},
+		config = function()
+			-- stop putting a giant window over my editor
+			vim.g.fzf_layout = { down = '~20%' }
+			-- when using :Files, pass the file list through
+			--
+			--   https://github.com/jonhoo/proximity-sort
+			--
+			-- to prefer files closer to the current file.
+			function list_cmd()
+				local base = vim.fn.fnamemodify(vim.fn.expand('%'), ':h:.:S')
+				if base == '.' then
+					-- if there is no current file,
+					-- proximity-sort can't do its thing
+					return 'fd --type file --follow -H'
+				else
+					return vim.fn.printf('fd --type file --follow -H | proximity-sort %s', vim.fn.shellescape(vim.fn.expand('%')))
+				end
+			end
+			vim.api.nvim_create_user_command('Files', function(arg)
+				vim.fn['fzf#vim#files'](arg.qargs, { source = list_cmd(), options = '--tiebreak=index' }, arg.bang)
+			end, { bang = true, nargs = '?', complete = "dir" })
 		end
 	},
 	-- LSP
