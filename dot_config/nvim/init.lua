faea19592866382036f88c3fa66931c823506fcf
Graeme Read <graeme@sigma957.net>
2024-03-17 18:40:37 +0000

    Make it clearly visible which argument we are at

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -217,6 +217,10 @@ require("lazy").setup({
 			-- Make comments more prominent -- they are important.
 			local bools = vim.api.nvim_get_hl(0, { name = 'Boolean' })
 			vim.api.nvim_set_hl(0, 'Comment', bools)
+
+			-- Make it clearly visible which argument we're at.
+			local marked = vim.api.nvim_get_hl(0, { name = 'PMenu' })
+			vim.api.nvim_set_hl(0, 'LspSignatureActiveParameter', { fg = marked.fg, bg = marked.bg, ctermfg = marked.ctermfg, ctermbg = marked.ctermbg, bold = true })
 		end
 	},
 	-- nice bar at the bottom
