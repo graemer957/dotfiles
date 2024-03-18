c3350fa15306e60a877deb8363bfa15e065b52a2
Graeme Read <graeme.read@mnscorp.net>
2024-03-18 13:30:15 +0000

    Disable the default providers in nvim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -501,3 +501,13 @@ require("lazy").setup({
 	},
 })
 
+-------------------------------------------------------------------------------
+--
+-- custom configuration
+--
+-------------------------------------------------------------------------------
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_node_provider = 0
+
