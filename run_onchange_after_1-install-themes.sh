0716098925b987aaacdd59cfa07eaf49875557ea
Graeme Read <graeme@sigma957.net>
2024-09-18 14:51:30 +0100

    Update chezmoi install themes script
    Do not blindly delete what we already have!
    
    Check out a specific commit we know is working

--- run_onchange_after_1-install-themes.sh
+++ run_onchange_after_1-install-themes.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-mkdir -p ~/dev/others/base16/templates 
-cd ~/dev/others/base16/templates
+mkdir -p ~/dev/others/base16/templates
+cd ~/dev/others/base16/templates || exit
 
-rm -rf fish-shell
-git clone gh:tomyun/base16-fish.git fish-shell
+if [ ! -d "fish-shell" ]; then
+	(
+		git clone gh:tomyun/base16-fish.git fish-shell
+		cd fish-shell || exit
+		git checkout 2f6dd97
+	)
+fi
 
-rm -rf tmux
-git clone gh:tinted-theming/base16-tmux.git tmux
+if [ ! -d "tmux" ]; then
+	(
+		git clone gh:tinted-theming/base16-tmux.git tmux
+		cd tmux || exit
+		git checkout 49a493e
+	)
+fi
