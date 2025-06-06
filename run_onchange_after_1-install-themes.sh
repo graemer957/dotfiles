124528d73e2995e8f20972bc643f23a6d8bde1f4
Graeme Read <graeme@sigma957.net>
2025-06-06 14:09:55 +0100

    Use HTTPS for retriving theming for fish and tmux
    Sequencing on a new machine, does not support it and we don't loose anything

--- run_onchange_after_1-install-themes.sh
+++ run_onchange_after_1-install-themes.sh
@@ -5,7 +5,7 @@ cd ~/dev/others/base16/templates || exit
 
 if [ ! -d "fish-shell" ]; then
 	(
-		git clone gh:tomyun/base16-fish.git fish-shell
+		git clone https://github.com/tomyun/base16-fish.git fish-shell
 		cd fish-shell || exit
 		git checkout 2f6dd97
 	)
@@ -13,7 +13,7 @@ fi
 
 if [ ! -d "tmux" ]; then
 	(
-		git clone gh:tinted-theming/base16-tmux.git tmux
+		git clone https://github.com/tinted-theming/base16-tmux.git tmux
 		cd tmux || exit
 		git checkout 49a493e
 	)
