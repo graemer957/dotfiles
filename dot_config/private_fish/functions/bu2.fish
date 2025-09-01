cf81ced82ed2c42237377b0951f29e7248ebe0cb
Graeme Read <graeme.read@mnscorp.net>
2025-09-01 08:49:11 +0100

    Update backup script to use new machine

--- dot_config/private_fish/functions/bu2.fish
+++ dot_config/private_fish/functions/bu2.fish
@@ -1,6 +1,5 @@
 function bu2
-    rsync -avzh --delete Code switch:MBH102258
-    rsync -avzh --delete Downloads switch:MBH102258
-    rsync -avzh --delete dev switch:MBH102258
-    rsync -avzh --delete rustlings switch:MBH102258
+    rsync -avzh --delete Downloads trinity:MBH102258
+    rsync -avzh --delete dev trinity:MBH102258
+    rsync -avzh --delete rustlings trinity:MBH102258
 end
