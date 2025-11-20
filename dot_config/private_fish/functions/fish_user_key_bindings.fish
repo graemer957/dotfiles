d70cf02b3f6eea870187b30628ae994c48721b66
Graeme Read <graeme@sigma957.net>
2025-11-20 21:49:48 +0000

    Restore fish v3 behaviour with Alt+Backspace

--- dot_config/private_fish/functions/fish_user_key_bindings.fish
+++ dot_config/private_fish/functions/fish_user_key_bindings.fish
@@ -6,4 +6,7 @@ function fish_user_key_bindings
     if command -v zoxide > /dev/null
         zoxide init fish | source
     end
+
+    # Restore fish v3 behavior: Alt+Backspace deletes a word, not an entire argument
+    bind alt-backspace backward-kill-word
 end
