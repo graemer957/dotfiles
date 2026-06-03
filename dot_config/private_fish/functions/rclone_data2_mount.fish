b4255f4107d7083a4e669c808eccb2ef4b7b32a6
Graeme Read <graeme@sigma957.net>
2026-06-03 11:10:48 +0100

    Remove lingering references to an old employer

--- dot_config/private_fish/functions/rclone_data2_mount.fish
+++
@@ -1,3 +0,0 @@
-function rclone_data2_mount -d "Mount data2: from `rclone serve`"
-    mount -oport=9572,mountport=9572 localhost: ~/data2
-end
--- dot_config/private_fish/functions/rclone_data2_mount_l.fish
+++
@@ -1,3 +0,0 @@
-function rclone_data2_mount_l -d "Mount data2: from r2"
-    rclone_data2_env mount data2: /mnt/data2 -v
-end
